1d33efa391d90d72a32686e8171b182993455b27
naelchamssiddine-670 <naelchamssiddine@gmail.com>
2025-12-04 16:03:58 +0300

    on n'a fait un jeu gestion de bibliotheque qui contennent 500 livres

---
+++ script.js
@@ -0,0 +1,67 @@
+// Ici, je rédige mon script JavaScript//
+let nom ="Chamssiddine";
+let prenom ="Nael";
+let age = 19;
+console.log("Bonjour, je m'appelle ",prenom, nom);
+console.log("J'ai ",age);
+let prix = 12;
+const nombreDecimal = 3.4123;
+const nombreNegatif = -509;
+const nombreFraction = 1/3;
+
+console.log("prix:", prix);
+console.log("nombre decimal",nombreDecimal);
+console.log("nombre fraction",nombreFraction);
+        console.log("nombre negatif",nombreNegatif);
+
+        let salutations = "Bonjour les gens";
+        let greeting = "Good morning";
+        let andabu = "Kwezi";
+
+        console.log(salutations);
+        console.log(greeting);
+        console.log(andabu);
+
+        let commune = "M'tsamboro";
+        let village ='M\'tsahara';
+
+        console.log("Je suis de la commune de", commune);
+        console.log("et je voudrais le village de", village);
+        
+        let vrai = true;
+        let est_faux = false;
+
+        console.log(vrai);
+        console.log(est_faux);
+
+        let eleves = ["Said","Amina","Bao"];
+        console.log(eleves);
+
+        let diplome = [true , 10 , "Daniel"];
+
+        console.log(eleves[0]);
+        console.log(eleves[1]);
+        console.log(eleves[2]);
+
+        console.log(diplome);
+        console.log(diplome[1]);
+        console.log(diplome[2]);
+
+        let tableauEleves = ["Amina","Bacar","Salim","Abdou"];
+
+        let client = {
+            nom: "Bacar",
+            village: "Choungui",
+            age: 25,
+            scores: [4, 7, 2]
+        };
+
+        console.log(client);
+
+        console.log(client.nom);//récupère Bacar
+        console.log(client.village);//récupère Choungui
+        console.log(client.age);//récupère 25
+        console.log(client.scores);//récupère [4, 7, 2]
+        console.log(client.scores[1]);//récupère 7
+
+
