cae7768cbc3adbdffc9a364efb297f442a447ec3
naelchamssiddine-670 <naelchamssiddine@gmail.com>
2025-12-11 11:24:25 +0300

    création d'un jeu de calcul

---
+++ calculAge.js
@@ -0,0 +1,22 @@
+function calculAge() {
+    const anneeNaissnce = document.getElementById("anneeNaissance").Value;
+    const ageAffichage = document.getElementById("ageResultat");
+
+    if (anneNaissance && !isNaN(anneeNaissnce)) {
+        const anneCourante = new Date().getUTCFullYear();
+        const age = anneCourante - parseInt(anneeNaissnce);
+        ageAffichage.textContent = age + "ans";
+    } else {
+        ageAffichage.textContent = "";
+    }
+
+
+
+
+
+
+
+
+
+
+}
